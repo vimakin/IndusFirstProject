6b0d20568f1962e09f2b9cc5adf5dabc29c22b36
Makineni, Vinay Chandra <makineni271@gmail.com>
2024-07-02 22:23:24 -0400

    demo commit and push

---
+++ IndusFirstProject/src/main/java/Intro.java
@@ -0,0 +1,19 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Intro {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        myDog.sound();
+    }
+}
